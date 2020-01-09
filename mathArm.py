e9248790ae81d15725939bcb0b6a8b59f692fbc6
hama1185 <hamataku7332@softbank.ne.jp>
2020-01-09 15:09:50 +0900

    miss

--- mathArm.py
+++ mathArm.py
@@ -31,15 +31,23 @@ class controlArm():
         y4 = y ** 4
         z4 = z ** 4
         argumentX = -((2 * (self.j * self.l-self.j * y))/(self.j2-self.k2+self.l2+x2-2 * self.l * y+y2+z2))
-        argumentY = -(((self.j4 - 2 * self.j2 * self.k2 + self.k4 - 2  * self.j2 *  self.l2 - 2 * self.k2 * self.l2 + self.l4 + 2 * self.j2 * x2 - 2 * self.k2 * x2 + 2 * self.l2 * x2 + x4 + 4 * self.j2 * self.l * y + 4 * self.k2 * self.l * y - 4 * self.l3 * y - 4 * self.l * x2 * y - 2 * self.j2 * y2 - 2 * self.k2 * y2 + 6 * self.l2 * y2 + 2 * x2  * y2 - 4 * self.l * y3 + y4 + 2 * self.j2 * z2 - 2 * self.k2 * z2 + 2 * self.l2 * z2 + 2 * x2 * z2 - 4 * self.l * y * z2 + 2 * y2 * z2 + z4) ** 0.5 ) / ((self.j4 - 2 * self.j2 * self.k2 + self.k4 + 2 * self.j2 * self.l2 - 2 * self.k2 * self.l2 + self.l4 + 2 * self.j2 * x2 - 2 * self.k2 * x2 + 2 * self.l2 * x2+ x4 - 4 * self.j2 * self.l * y + 4 * self.k2 * self.l * y - 4 * self.l3 * y - 4 * self.l * x2 * y + 2 * self.j2 * y2 - 2 * self.k2 * y2 + 6 * self.l2 * y2 + 2 * x2 * y2 - 4 * self.l * y3 + y4 + 2 * self.j2 * z2 - 2 * self.k2 * z2 + 2 * self.l2 * z2 + 2 * x2 * z2 - 4 * self.l * y * z2 + 2 * y2 * z2 + z4) ** 0.5))
+        argumentY = -(((self.j4-2 * self.j2 * self.k2+self.k4-2 * self.j2 * self.l2-2 * self.k2 * self.l2+self.l4+2 * self.j2 * x2-2 * self.k2 * x2+2 * self.l2 * x2+x4+4 * self.j2 * self.l * y+4 * self.k2 * self.l * y-4 * self.l3 * y-4 * self.l * x2 * y-2 * self.j2 * y2-2 * self.k2 * y2+6 * self.l2 * y2+2 * x2 * y2-4 * self.l * y3+y4+2 * self.j2 * z2-2 * self.k2 * z2+2 * self.l2 * z2+2 * x2 * z2-4 * self.l * y * z2+2 * y2 * z2+z4) ** 0.5)/((self.j4-2 * self.j2 * self.k2+self.k4+2 * self.j2 * self.l2-2 * self.k2 * self.l2+self.l4+2 * self.j2 * x2-2 * self.k2 * x2+2 * self.l2 * x2+x4-4 * self.j2 * self.l * y+4 * self.k2 * self.l * y-4 * self.l3 * y-4 * self.l * x2 * y+2 * self.j2 * y2-2 * self.k2 * y2+6 * self.l2 * y2+2 * x2 * y2-4 * self.l * y3+y4+2 * self.j2 * z2-2 * self.k2 * z2+2 * self.l2 * z2+2 * x2 * z2-4 * self.l * y * z2+2 * y2 * z2+z4) ** 0.5))
         # b = math.atan2(argumentY,argumentX)
-        # print(b)
-        print(argumentX)
-        print(type(argumentY))
+        c = ((self.j4-2 * self.j2 * self.k2+self.k4+2 * self.j2 * self.l2-2 * self.k2 * self.l2+self.l4+2 * self.j2 * x2-2 * self.k2 * x2+2 * self.l2 * x2+x4-4 * self.j2 * self.l * y+4 * self.k2 * self.l * y-4 * self.l3 * y-4 * self.l * x2 * y+2 * self.j2 * y2-2 * self.k2 * y2+6 * self.l2 * y2+2 * x2 * y2-4 * self.l * y3+y4+2 * self.j2 * z2-2 * self.k2 * z2+2 * self.l2 * z2+2 * x2 * z2-4 * self.l * y * z2+2 * y2 * z2+z4) ** 0.5)
+        a = abs(((self.j4-2 * self.j2 * self.k2+self.k4-2 * self.j2 * self.l2-2 * self.k2 * self.l2+self.l4+2 * self.j2 * x2-2 * self.k2 * x2+2 * self.l2 * x2+x4+4 * self.j2 * self.l * y+4 * self.k2 * self.l * y-4 * self.l3 * y-4 * self.l * x2 * y-2 * self.j2 * y2-2 * self.k2 * y2+6 * self.l2 * y2+2 * x2 * y2-4 * self.l * y3+y4+2 * self.j2 * z2-2 * self.k2 * z2+2 * self.l2 * z2+2 * x2 * z2-4 * self.l * y * z2+2 * y2 * z2+z4) ** 0.5))
+        # print(math.degrees(b))
+        argumentX = -((2 * (self.j * self.l-self.j * y))/(self.j2-self.k2+self.l2+x2-2 * self.l * y+y2+z2))
+        argumentY = -(c/a)
         
+        b = math.atan2(argumentY,argumentX)
+        print(math.degrees(b))
 
     def controlThirdArm(x ,y ,z):
         print("c")
 
-classMethod = controlArm(50,50,50)
-classMethod.controlSecondArm(10,20,30)
+classMethod = controlArm(100,100,100)
+
+for i in range(-25,25):
+    for j in range(-25,25):
+        for k in range(-25,25):
+            classMethod.controlSecondArm(i,j,k)
